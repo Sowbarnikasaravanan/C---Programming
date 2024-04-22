76e43265934a7fdc7517c1985e80ec6e30988c56
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-04-22 20:30:55 +0530

    To get input from user and print it

---
+++ GettingInputFromUser.cpp
@@ -0,0 +1,14 @@
+// To get the input from the user and the print it.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number:";
+    cin >>n;
+    cout<<"You entered :"<<n<<endl;
+}
+/* 
+Sample output:
+Enter the number:12345
+You entered :12345
+*/
