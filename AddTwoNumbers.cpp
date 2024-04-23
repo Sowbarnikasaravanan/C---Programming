27db4e0ccc835c931841390997294cbf3a2360b3
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-04-23 21:53:48 +0530

    To add two numbers

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,20 @@
+// To add two numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    sum=a+b;
+    cout<<"The sum of two "<<a<<" and "<<b<<" is "<<sum<<endl;
+    return 0;
+}
+
+/*
+Sample output:
+Enter the first number:12
+Enter the second number:34
+The sum of two 12 and 34 is 46
+*/
