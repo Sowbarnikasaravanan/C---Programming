e1a06187f04fa13386201b0960e4451ed248cb24
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-04-24 21:21:44 +0530

    To subtract two numbers

---
+++ SubtractTwoNumbers.cpp
@@ -0,0 +1,19 @@
+// To subtract two numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,diff;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    diff=a-b;
+    cout<<"The difference between "<<a<<" and "<<b<<" is "<<diff;
+    return 0;
+}
+/*
+Sample output:
+Enter the first number:12
+Enter the second number:8
+The difference between 12 and 8 is 4
+*/
