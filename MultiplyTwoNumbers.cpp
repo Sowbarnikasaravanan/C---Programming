d4d6c338aaa3e0ff3c2306782a0ed8adc4974c56
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-04-25 23:37:33 +0530

    To multiply two numbers

---
+++ MultiplyTwoNumbers.cpp
@@ -0,0 +1,19 @@
+// To multiply two numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,mult;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    mult=a*b;
+    cout<<"The multiplication of "<<a<<" and "<<b<<" is "<<mult;
+    return 0;
+}
+/*
+Sample output:
+Enter the first number:12
+Enter the second number:8
+The difference between 12 and 8 is 96
+*/
