e971cf49ee0d1e6a9e90c5f3379e74df9815b040
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-04-29 23:14:52 +0530

    Divide two numbers

---
+++ DivideTwoNumbers.cpp
@@ -0,0 +1,20 @@
+// To divide a by b
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,div;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    div=a/b;
+    cout<<"The quotient when "<<a<<" divided by "<<b<<" is "<<div<<endl;
+    return 0;
+}
+
+/*
+Sample output:
+Enter the first number:36
+Enter the second number:12
+The quotient when 36 divided by 12 is 3
+*/
