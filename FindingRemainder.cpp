003fefb5b9b559d319bc3ebd26580a944cbd1f49
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-04-30 22:24:48 +0530

    To find the remainder of two numbers

---
+++ FindingRemainder.cpp
@@ -0,0 +1,9 @@
+// To find the remainder when a number divided by another number
+#include<iostream>
+using namespace std;
+void main(){
+    int a,b,rem;
+    cin>>a>>b;
+    rem=a%b;
+    cout<<rem;
+}
